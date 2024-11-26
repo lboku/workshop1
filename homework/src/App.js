b9cd081dac7dee36627e489ef9d5184c907f8682
lboku <129886726+lboku@users.noreply.github.com>
2024-11-26 21:33:35 +0400

    finish

---
+++ homework/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { products } from './constant/product';
+import (product)
+function App() {
+  return (
+    <main class="container">
+
+    </main>
+  );
+}
+
+export default App;
---
+++ homework/src/constant/product.js
@@ -0,0 +1,8 @@
+export const products = [
+  { title: "Product 1", alt: "Image-1" },
+  { title: "Product 2", alt: "Image-1" },
+  { title: "Product 3", alt: "Image-1" },
+  { title: "Product 1", alt: "Image-1" },
+  { title: "Product 2", alt: "Image-1" },
+  { title: "Product 3", alt: "Image-1" },
+]
